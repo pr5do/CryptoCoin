289cf8efe55b61f43c03ff240becfdd98cbaafc9
Gustavo Prado <github.v5x7y@aleeas.com>
2021-12-27 13:34:48 -0300

    Removing the tests folder
    Revert

--- tests/test-mysql.py
+++
@@ -1,8 +0,0 @@
-from database_manager import store_user
-import getpass
-
-user = input("Provide your username: ").replace(" ", "")
-
-passwd = getpass.getpass("Provide your password: ").replace(" ", "")
-
-store_user(user, passwd)
