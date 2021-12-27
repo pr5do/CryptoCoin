da1f93d4c1e20d9f0f18a200e88596a253b6b922
Gustavo Prado <github.v5x7y@aleeas.com>
2021-12-26 22:18:10 -0300

    Elaborando o sistema de login para a aplicação final

--- tests/test-mysql.py
+++ tests/test-mysql.py
@@ -1,54 +1,15 @@
-'''from sys import exit
-from os import name, system
-
-users = {}
-status = ''
-
-def clear():
-    if name == 'nt':       #If Windows clear screen
-        _ = system('cls')
-    else:                  #If Linux etc. clear screen
-        _ = system('clear')
-
-def newUser():
-    newUsername = input('Create new username: ')
-
-    if newUsername in users:
-        print('\nUsername already exists!\n')
-        input('Press [Enter] to continue')
-    else:
-        newPassword = input('Create new password: ')
-        users[newUsername] = newPassword #Append to dictionary(users)
-        print('\nAccount created successfully!\n')
-        input('Press [Enter] to continue')
-
-def oldUser():
-    login = input('Enter username: ')
-    password = input('Enter password: ')
-    if login in users and users[login] == password: #If user and pass match, continue
-        print('\nLogin successful!\n')
-        input('Press [Enter] to continue')
-    else:
-        print('\nLogin failed!\n')
-        input('Press [Enter] to continue')
-
-while True:
-    clear()
-    status = input('Are you a registered user? [y/n] Press [q] to quit\n> ')
-    status = status.lower()
-    if status == 'y':
-        oldUser()
-    elif status == 'n':
-        newUser()
-    elif status == 'q':
-        exit()
-    else:
-        continue'''
-
 import mysql.connector
-mydb = mysql.connector.connect(
+
+db = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="4kD1F,MjK-)ZScYTa4En@4EO5[&piX}w1_jv"
+    passwd="4kD1F,MjK-)ZScYTa4En@4EO5[&piX}w1_jv",
+    database="cryptocoin"
 )
-print(mydb)
+
+mycursor = db.cursor()
+
+mycursor.execute('desc login')
+
+for x in mycursor:
+    print(x)
