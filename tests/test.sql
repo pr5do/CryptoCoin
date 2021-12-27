ff10d39f2b328a421b94982bedb818b2c9b5b02d
Gustavo Prado <github.v5x7y@aleeas.com>
2021-12-27 13:34:48 -0300

    Revert

--- tests/test.sql
+++ tests/test.sql
@@ -1,1 +1,9 @@
-show tables;
+create table login(
+	id int not null auto_increment,
+	`user` varchar(30) not null unique,
+    passwd text not null,
+    primary key (id)
+)default charset = utf8;
+
+alter table login
+modify user varchar(100);
